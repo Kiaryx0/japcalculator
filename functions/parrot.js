4b84628931734c3c8563fbf72f5caa3a687f06e6
Maxime Tran <33542706+Kiaryx0@users.noreply.github.com>
2020-10-27 11:00:29 +0100

    update parrot

--- functions/parrot.js
+++ functions/parrot.js
@@ -1,15 +1,15 @@
 exports.handler = async function (event, context) {
     const requestBody = JSON.parse(event.body);
     const message = requestBody.message;
-    if (message == NULL) {
+    if (message) {
         return {
             statusCode: 200,
-            body: JSON.stringify({ message: "Pas de message. " }),
+            body: JSON.stringify({ message: message }),
         };
     } else {
         return {
             statusCode: 400,
-            body: JSON.stringify({ message: message }),
+            body: JSON.stringify({ message: 'No.' }),
         };
     }
 };
